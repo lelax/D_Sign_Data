ad9d6f809ccb61e8c759e93212f17c278f4682cf
Camila Oliveira <camila.oliveira@studio.unibo.it>
2022-05-24 22:18:45 +0200

    Update importCSV.py

--- importCSV.py
+++ importCSV.py
@@ -1,8 +1,15 @@
 import csv
 from csv import reader
 
+from sqlalchemy import true
+
 with open("../D_Sign_Data/import/relational_publications.csv", "r", encoding="utf-8") as f:
     publications = reader(f)
     data = csv.reader(f)
     for row in data:
         print(row)
+
+def check(data):
+    for i in data:
+        if i in 'aeiou':
+            return true
