4d0a3607353d463fdf8611a0c7d3e143a6a83a52
anitavishinskaite <92692699+anitavishinskaite@users.noreply.github.com>
2022-07-13 23:34:33 +0200

    Update graph_01.py

--- graph_01.py
+++ graph_01.py
@@ -1,79 +1,70 @@
-from sys import orig_argv
-from rdflib import Graph
-
-from impl import Organization, Publication, Venue
-
-my_graph = Graph()
-
-from rdflib import URIRef
-
-# classes of resources
-Person = URIRef("https://schema.org/Person")
-JournalArticle = URIRef("https://schema.org/ScholarlyArticle")
-BookChapter = URIRef("https://schema.org/Chapter")
-ProceedingsPaper = URIRef("https://schema.org/ScholarlyArticle")
-Journal = URIRef("https://schema.org/Periodical")
-Book = URIRef("https://schema.org/Book")
-Proceedings = URIRef("https://schema.org/Legislation")
-
-
-# attributes related to classes
-doi = URIRef("https://schema.org/identifier")
-publicationYear = URIRef("https://schema.org/datePublished")
-MostCitedPublication = URIRef()
-MostCitedVenues = URIRef()
-title = URIRef("https://schema.org/name")
-issue = URIRef("https://schema.org/issueNumber")
-volume = URIRef("https://schema.org/volumeNumber")
-identifier = URIRef("https://schema.org/identifier")
-name = URIRef("https://schema.org/name")
-
-# relations among classes
-publicationVenue = URIRef("https://schema.org/isPartOf")
-
-import pandas as pd
-from rdflib import Literal
-
-a_string = Literal("a string")
-a_number = Literal(3)
-a_boolean = Literal(True)
-
-
 from pandas import read_csv, Series
 from rdflib import RDF
 
-# This is the string defining the base URL used to defined
-# the URLs of all the resources created from the data
 base_url = "https://github.com/lelax/D_Sign_Data"
 
-venues = read_csv("../graph_publications.csv", 
-                  keep_default_na=False,
-                  dtype={
-                      "id": "string",
-                      "name": "string",
-                      "type": "string"
+publications = read_csv("../D_Sign_Data-1\import\graph_publications.csv", 
+                 keep_default_na=False,
+                 dtype={
+                     "id": "string",
+                     "title": "string",
+                     "type": "string",
+                     "publication_year": "int",
+                     "publication_venue": "string",
+                     "issue": "string",
+                     "volume": "string",
+                     "chapter": "string",
                   })
 
-venue_internal_id = {}
-for idx, row in venues.iterrows():
-    local_id = "venue-" + str(idx)
+for idx, row in publications.iterrows():
+    local_id = "publication-" + str(idx)
     
-
     subj = URIRef(base_url + local_id)
+
+    if row["type"] == "journal-article":
+        my_graph.add((subj, RDF.type, JournalArticle))
+        # These two statements applies only to journal articles
+        my_graph.add((subj, issue, Literal(row["issue"])))
+        my_graph.add((subj, volume, Literal(row["volume"])))
+    elif row["type"] == "book-chapter":
+        my_graph.add((subj, RDF.type, BookChapter))
+        #This statement applies only to book chapters
+        my_graph.add((subj, chapterNumber, Literal(row["chapter"])))
+    else: 
+        my_graph.add((subj, RDF.type, ProceedingsPaper))
+
+    my_graph.add((subj, cites, Publication))
+    my_graph.add((subj, title, Literal(row["title"])))
+    my_graph.add((subj, publicationYear, Literal(str(row["publication_year"]))))
+    my_graph.add((subj, publicationVenue, Literal(row["publication_venue"])))
+    my_graph.add((subj, identifier, Literal(str(row["id"]))))
+
+
+
+venues = read_csv("../D_Sign_Data-1\import\graph_publications.csv", 
+                 keep_default_na=False,
+                 dtype={
+                     "venue_type": "string",
+                     "publisher": "string",
+                     "event": "string"
+                  })
+
+for idx, row in venues.iterrows():
+    local_id = "venues-" + str(idx)
     
-    # We put the new venue resources created here, to use them
-    # when creating publications
-    venue_internal_id[row["id"]] = subj
-    
-    if row["type"] == "journal":
-        # RDF.type is the URIRef already provided by rdflib of the property 
-        # 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+    subj = URIRef(base_url + local_id)
+
+    if row["venue_type"] == "journal":
         my_graph.add((subj, RDF.type, Journal))
+    elif row["venue_type"] == "book":
+        my_graph.add((subj, RDF.type, BookChapter))
     else:
-        my_graph.add((subj, RDF.type, Book))
-    
-    my_graph.add((subj, name, Literal(row["name"])))
-    my_graph.add((subj, identifier, Literal(row["id"])))
+        my_graph.add((subj, RDF.type, Proceedings))
+        #This statement applies only to proceedings
+        my_graph.add((subj, event, Literal(row["event"])))
+
+    my_graph.add((subj, publisher, Literal(row["publisher"])))
+
  
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 
